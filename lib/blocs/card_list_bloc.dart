febe5c4334e419b8f7d6149040eb8a7b05f0740f
marcelo <marcelo.tk3@gmail.com>
2019-03-22 11:02:56 -0500

    card_List_bloc

---
+++ lib/blocs/card_list_bloc.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'package:rxdart/rxdart.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:card_wallet/models/card_model.dart';
+import 'dart:convert';
+import 'package:card_wallet/helpers/card_colors.dart';
+
+class CardListBloc {
+  BehaviorSubject<List<CardResults>> _cardsCollection = BehaviorSubject<List<CardResults>>();
+
+  List<CardResults> _cardResults;
+
+  //Retrieve
+  Stream<List<CardResults>> get cardsList => _cardsCollection.stream;
+
+  void initialData() async {
+    var initialData = await rootBundle.loadString('data/initialData.json');
+    var decodeJson = jsonDecode(initialData);
+    _cardResults = CardModel.fromJson(decodeJson).results;
+
+    for(var i = 0; i < _cardResults.length ; i++) {
+      _cardResults[i].cardColor = CardColors.baseColors[i];
+    }
+  }
+
+  CardListBloc(){
+    initialData();
+  }
+
+  void dispose(){
+    _cardsCollection.close();
+  }
+}
+
+final cardListBloc = CardListBloc();
